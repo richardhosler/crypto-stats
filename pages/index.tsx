82e7bb534da9ee3d8f05e85ab27a8d080c363105
Richard Hosler <kaltaugh@gmail.com>
2022-04-28 18:40:23 +0100

    feat: Plot added

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,15 @@ import { useState, useEffect } from "react";
 import Head from "next/head";
 import { SymbolSelector } from "../components/SymbolSelector";
 import { useHourlyQuery } from "../hooks/useHourly";
+import { formatHourlyQueryData } from "../utils/formatHourlyQueryData";
+import { Flex } from "@chakra-ui/react";
+import dynamic from "next/dynamic";
+const Plot = dynamic(() => import("../components/PlotArea"), {
+	ssr: false,
+});
+function PlotAreaDynamic({ data }) {
+	return <Plot data={data} />;
+}
 
 export default function Home() {
 	const [fsym, setFsym] = useState(["BTC"]);
@@ -14,11 +23,15 @@ export default function Home() {
 	}, []);
 
 	useEffect(() => {
-		queryData && console.log({ qd: queryData });
+		queryData && console.log(formatHourlyQueryData(queryData));
 	}, [queryData]);
 
+	const PlotAreaProps = {
+		data: formatHourlyQueryData(queryData),
+	};
+
 	return (
-		<div>
+		<Flex>
 			<Head>
 				<title>Crypto Stats</title>
 				<meta name="description" content="Cryptocurrency stats page" />
@@ -29,11 +42,12 @@ export default function Home() {
 				setSymList={setFsym}
 				hasMounted={hasMounted}
 			/>
+			<PlotAreaDynamic data={formatHourlyQueryData(queryData)} />
 			<SymbolSelector
 				symbol={tsym}
 				setSymbol={setTsym}
 				hasMounted={hasMounted}
 			/>
-		</div>
+		</Flex>
 	);
 }
