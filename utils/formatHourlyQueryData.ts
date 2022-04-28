e7adb4ed6e100b2e340b9834701e2a0911aede20
Richard Hosler <kaltaugh@gmail.com>
2022-04-28 18:39:56 +0100

    fix: timestamp negative from current

--- utils/formatHourlyQueryData.ts
+++ utils/formatHourlyQueryData.ts
@@ -3,12 +3,13 @@ interface Point {
 	y: number;
 }
 export const formatHourlyQueryData = (data: any) => {
+	let now = new Date().getTime();
 	let formattedData = [];
 	data.forEach((d) => {
 		let points = [];
 
 		d.Data.Data.forEach((dd) => {
-			points.push({ x: dd.time, y: dd.close });
+			points.push({ x: dd.time - now, y: dd.close });
 		});
 		formattedData.push(points);
 	});
