7bb2da657d95b95a600db75272dc71973cd26183
Richard Hosler <kaltaugh@gmail.com>
2022-04-29 18:50:16 +0100

    Update formatHourlyQueryData.ts

--- utils/formatHourlyQueryData.ts
+++ utils/formatHourlyQueryData.ts
@@ -1,16 +1,17 @@
 export const formatHourlyQueryData = (data: any) => {
-	let formattedData = [];
-	data.forEach((d) => {
-		let points = [];
-		let st = BigInt(new Date().getTime());
-		d.Data.Data.forEach((dd, key) => {
-			key == 0 ? (st = BigInt(dd.time).valueOf()) : st;
-			points.push({
-				x: Number((BigInt(dd.time).valueOf() - st) / 3600n),
-				y: Number(Math.floor(dd.close)),
-			});
-		});
-		formattedData.push(points);
-	});
-	return formattedData;
+  let formattedData: { x: number; y: number }[][] = [];
+  data.forEach((d) => {
+    let points = [];
+    let st = BigInt(new Date().getTime());
+    d.Data.Data.forEach((dd, key) => {
+      key == 0 ? (st = BigInt(dd.time).valueOf()) : st;
+      points.push({
+        x: Number((BigInt(dd.time).valueOf() - st) / 3600n),
+        y: Number(Math.floor(dd.close)),
+      });
+    });
+    formattedData.push(points);
+  });
+
+  return formattedData;
 };
