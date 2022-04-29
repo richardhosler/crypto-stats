5e0ab24f2bacf31ba190b867540efebc84f1d2fc
Richard Hosler <kaltaugh@gmail.com>
2022-04-29 18:50:48 +0100

    Update formatHourlyQueryData.ts

--- utils/formatHourlyQueryData.ts
+++ utils/formatHourlyQueryData.ts
@@ -1,17 +1,28 @@
-export const formatHourlyQueryData = (data: any) => {
-  let formattedData: { x: number; y: number }[][] = [];
-  data.forEach((d) => {
-    let points = [];
-    let st = BigInt(new Date().getTime());
-    d.Data.Data.forEach((dd, key) => {
-      key == 0 ? (st = BigInt(dd.time).valueOf()) : st;
-      points.push({
-        x: Number((BigInt(dd.time).valueOf() - st) / 3600n),
-        y: Number(Math.floor(dd.close)),
-      });
+import { useQueries } from "react-query";
+
+export const useHourlyQuery = (fsym: string[], tsym: string) => {
+  let queryOptions = [];
+
+  fsym.forEach((symbol) => {
+    queryOptions.push({
+      queryKey: ["hourly", symbol],
+      queryFn: (qopt) => {
+        const url = `https://min-api.cryptocompare.com/data/v2/histohour?tryConversion=true&fsym=${qopt.queryKey[1]}&tsym=${tsym}&limit=24&aggregate=1&apikey=${process.env.NEXT_PUBLIC_CRYPTO_COMPARE_API_KEY}`;
+        return fetch(url).then((response) => response.json());
+      },
     });
-    formattedData.push(points);
   });
 
-  return formattedData;
+  let data = useQueries(queryOptions);
+  let output = [];
+
+  data.forEach((d) => {
+    if (d.data) {
+      output.push(d.data);
+    }
+  }, []);
+
+  // TODO: Adding interface to queryFn
+  // Change all foreach for map ideally
+  return output;
 };
