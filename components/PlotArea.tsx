491a1a6982f4ef65ac92bfb18bed9dad230f2e76
Richard Hosler <kaltaugh@gmail.com>
2022-04-29 18:48:53 +0100

    chore: refactoring

--- components/PlotArea.tsx
+++ components/PlotArea.tsx
@@ -1,43 +1,49 @@
+import { useMemo } from "react";
 import { VictoryAxis, VictoryChart, VictoryLine } from "victory";
+import { formatHourlyQueryData } from "../utils/formatHourlyQueryData";
 
 const PlotArea = (data) => {
-	if (!data) return null;
-	console.log(data);
+  const chartData = useMemo(() => {
+    if (!data) return [];
 
-	return (
-		<VictoryChart
-			domain={{ x: [-0.5, 12.5] }}
-			domainPadding={{ y: [50, 50] }}
-			animate={{ duration: 500 }}
-		>
-			<VictoryAxis
-				label="Time"
-				style={{
-					axis: { stroke: "black" },
-					axisLabel: { fontSize: 12, padding: 30 },
-					grid: { stroke: "grey", strokeWidth: 1 },
-					tickLabels: { fontSize: 5, padding: 5 },
-				}}
-			/>
-			<VictoryAxis
-				dependentAxis
-				label="Price"
-				style={{
-					axis: { stroke: "black" },
-					axisLabel: { fontSize: 12, padding: 30 },
-					tickLabels: { fontSize: 5, padding: 5 },
-				}}
-			/>
-			{data[0] && (
-				<VictoryLine
-					style={{
-						data: { stroke: "#c43a31" },
-						parent: { border: "1px solid #ccc" },
-					}}
-					data={data[0]}
-				/>
-			)}
-		</VictoryChart>
-	);
+    return formatHourlyQueryData(data.data);
+  }, [data]);
+
+  console.log({ chartData });
+
+  return (
+    <VictoryChart domainPadding={{ y: [50, 50] }} animate={{ duration: 500 }}>
+      <VictoryAxis
+        label="Time"
+        style={{
+          axis: { stroke: "black" },
+          axisLabel: { fontSize: 12, padding: 30 },
+          grid: { stroke: "grey", strokeWidth: 1 },
+          tickLabels: { fontSize: 5, padding: 5 },
+        }}
+      />
+      <VictoryAxis
+        dependentAxis
+        label="Price"
+        style={{
+          axis: { stroke: "black" },
+          axisLabel: { fontSize: 12, padding: 30 },
+          tickLabels: { fontSize: 5, padding: 5 },
+        }}
+      />
+      {chartData &&
+        chartData.map((series, key) => (
+          <VictoryLine
+            key={key}
+            style={{
+              data: { stroke: "#c43a31" },
+              parent: { border: "1px solid #ccc" },
+            }}
+            data={series}
+          />
+        ))}
+    </VictoryChart>
+  );
 };
+
 export default PlotArea;
