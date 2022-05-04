abda2557f0e1a0e235617757aa90f8e0cee663ad
Richard Hosler <kaltaugh@gmail.com>
2022-05-04 15:20:55 +0100

    chore: rename use Hourly

--- hooks/useHourlyQuery.ts
+++ hooks/useHourlyQuery.ts
@@ -1,18 +1,22 @@
 import { useQueries } from "react-query";
-
+// TODO: Adding interface to queryFn
+interface QueryOptionsInterface {
+	queryKey: string[] | string;
+	queryFn: (qopt: any) => Promise<any>;
+}
 export const useHourlyQuery = (fsym: string[], tsym: string) => {
-	let queryOptions = [];
+	let queryOptions: QueryOptionsInterface[] = [];
 
 	fsym.map((symbol) => {
 		queryOptions.push({
 			queryKey: ["hourly", symbol],
-			queryFn: (qopt) => {
+			queryFn: (qopt): Promise<any> => {
 				const url = `https://min-api.cryptocompare.com/data/v2/histohour?tryConversion=true&fsym=${qopt.queryKey[1]}&tsym=${tsym}&limit=24&aggregate=1&apikey=${process.env.NEXT_PUBLIC_CRYPTO_COMPARE_API_KEY}`;
 				return fetch(url).then((response) => response.json());
 			},
 		});
 	});
-	let data = useQueries(queryOptions);
+	const data = useQueries(queryOptions);
 	let output = [];
 
 	data.map((d) => {
@@ -21,6 +25,5 @@ export const useHourlyQuery = (fsym: string[], tsym: string) => {
 		}
 	}, []);
 
-	// TODO: Adding interface to queryFn
 	return output;
 };
