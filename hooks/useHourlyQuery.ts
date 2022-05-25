7bab5015de549164852eb709c7711176e46b065d
Richard Hosler <kaltaugh@gmail.com>
2022-05-25 11:01:31 +0100

    chore: added interfaces for incoming data

--- hooks/useHourlyQuery.ts
+++ hooks/useHourlyQuery.ts
@@ -1,29 +1,55 @@
-import { useQueries } from "react-query";
+import { QueriesOptions, useQueries } from "react-query";
 // TODO: Adding interface to queryFn
 interface QueryOptionsInterface {
-  queryKey: string[] | string;
-  queryFn: (qopt: any) => Promise<any>;
+	queryKey: string[] | string;
+	queryFn: (qopt: any) => Promise<Root>;
+}
+export interface Root {
+	Response: string;
+	Message: string;
+	HasWarning: boolean;
+	Type: number;
+	RateLimit: RateLimit;
+	Data: Data;
+}
+interface RateLimit {}
+interface Data {
+	Aggregated: boolean;
+	TimeFrom: number;
+	TimeTo: number;
+	Data: Daum[];
+}
+interface Daum {
+	time: number;
+	high: number;
+	low: number;
+	open: number;
+	volumefrom: number;
+	volumeto: number;
+	close: number;
+	conversionType: string;
+	conversionSymbol: string;
 }
 export const useHourlyQuery = (fsym: string[], tsym: string) => {
-  let queryOptions: QueryOptionsInterface[] = [];
+	let queryOptions: QueryOptionsInterface[] = [];
 
-  fsym.map((symbol) => {
-    queryOptions.push({
-      queryKey: ["hourly", symbol, tsym],
-      queryFn: (qopt): Promise<any> => {
-        const url = `https://min-api.cryptocompare.com/data/v2/histohour?tryConversion=true&fsym=${qopt.queryKey[1]}&tsym=${tsym}&limit=24&aggregate=1&apikey=${process.env.NEXT_PUBLIC_CRYPTO_COMPARE_API_KEY}`;
-        return fetch(url).then((response) => response.json());
-      },
-    });
-  });
-  const data = useQueries(queryOptions);
-  let output = [];
+	fsym.map((symbol) => {
+		queryOptions.push({
+			queryKey: ["hourly", symbol, tsym],
+			queryFn: (qopt): Promise<Root> => {
+				const url = `https://min-api.cryptocompare.com/data/v2/histohour?tryConversion=true&fsym=${qopt.queryKey[1]}&tsym=${tsym}&limit=24&aggregate=1&apikey=${process.env.NEXT_PUBLIC_CRYPTO_COMPARE_API_KEY}`;
+				return fetch(url).then((response) => response.json());
+			},
+		});
+	});
+	const data = useQueries(queryOptions);
+	let output = [];
 
-  data.map((d) => {
-    if (d.data) {
-      output.push(d.data);
-    }
-  }, []);
+	data.map((d) => {
+		if (d.data) {
+			output.push(d.data);
+		}
+	}, []);
 
-  return output;
+	return output;
 };
--- utils/formatHourlyQueryData.ts
+++ utils/formatHourlyQueryData.ts
@@ -1,63 +1,38 @@
+import { Root } from "../hooks/useHourlyQuery";
+
 export interface FormattedDataInterface {
-  close: number;
-  high: number;
-  low: number;
-  open: number;
-  time: number;
+	close: number;
+	high: number;
+	low: number;
+	open: number;
+	time: number;
 }
 export const formatHourlyQueryData = (
-  data: any
+	roots: Root[]
 ): FormattedDataInterface[][] => {
-  let formattedData: FormattedDataInterface[][] = [];
+	let formattedData: FormattedDataInterface[][] = [];
 
-  // TODO: https://transform.tools/json-to-typescript
-  data.map((d: { Data: { Data: any[] } }) => {
-    let series: FormattedDataInterface[] = [];
-    let st = BigInt(new Date().getTime()); // TODO: Try to avoid shorthand notation
+	roots.map((d: Root) => {
+		let series: FormattedDataInterface[] = [];
+		let startTime = BigInt(new Date().getTime());
 
-    d.Data.Data.map((dd, key) => {
-      key == 0 ? (st = BigInt(dd.time)) : st;
-      series.push({
-        time: Number((BigInt(dd.time) - st) / 3600n) - 24,
-        open: dd.open,
-        close: dd.close,
-        high: dd.high,
-        low: dd.low,
-      });
-    });
+		d.Data.Data.map((dd, key) => {
+			key == 0 ? (startTime = BigInt(dd.time)) : startTime;
+			series.push({
+				time: Number((BigInt(dd.time) - startTime) / 3600n) - 24,
+				open: dd.open,
+				close: dd.close,
+				high: dd.high,
+				low: dd.low,
+			});
+		});
 
-    formattedData.push(series);
-  });
+		formattedData.push(series);
+	});
 
-  return formattedData;
+	return formattedData;
 };
 
-// export interface Root {
-// 	Response: string
-// 	Message: string
-// 	HasWarning: boolean
-// 	Type: number
-// 	RateLimit: RateLimit
-// 	Data: Data
-//   }
+// export interface
 
-//   export interface RateLimit {}
-
-//   export interface Data {
-// 	Aggregated: boolean
-// 	TimeFrom: number
-// 	TimeTo: number
-// 	Data: Daum[]
-//   }
-
-//   export interface Daum {
-// 	time: number
-// 	high: number
-// 	low: number
-// 	open: number
-// 	volumefrom: number
-// 	volumeto: number
-// 	close: number
-// 	conversionType: string
-// 	conversionSymbol: string
-//   }
+//   export
